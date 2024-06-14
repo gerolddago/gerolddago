66da80d2cedcd08ff6c5f211635048260d7b8a6f
gerolddago <115063769+gerolddago@users.noreply.github.com>
2024-06-14 12:55:01 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var icon = document.querySelector("#icon");
-    icon.onclick = function(){
-      document.body.classList.toggle("dark-theme");
-      if(document.body.classList.contains("dark-theme")){
-        icon.src = "img/sun.png";
-      }else{
-        icon.src = "img/moon.png";
-      }
-    }
